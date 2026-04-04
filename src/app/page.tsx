b90ebb81cd32100d884e1c5adcf4a67b6303c3b5
Rajitpal Singh <singh.rajitpal@gmail.com>
2026-04-04 18:44:59 +0200

    feat: initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="flex flex-1 flex-col items-center justify-center min-h-screen">
+      <h1 className="text-4xl font-bold tracking-tight">PortfolioForge</h1>
+    </main>
+  );
+}
